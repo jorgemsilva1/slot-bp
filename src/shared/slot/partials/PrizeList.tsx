8f2f40dc0eece109f62a23d4ed4329cf4211a5c8
guilhermebranco <guilherme@dvagar.cc>
2025-05-26 10:07:55 +0100

    fix

--- src/shared/slot/partials/PrizeList.tsx
+++ src/shared/slot/partials/PrizeList.tsx
@@ -4,14 +4,14 @@ export const PrizeList = ({ arr }: { arr: any[] }) => {
     console.log(arr);
     return (
         <>
-            <div className={'blob'} style={{backgroundColor: '#232323', width: '25vw', height: '20vw', position:'absolute', zIndex: 99, top: '19vh', fontSize: '5rem', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+            <div className={'blob'} style={{backgroundColor: '#232323', width: '30vw', height: '25vw', position:'absolute', zIndex: 99, top: '19vh', fontSize: '6rem', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
                 Game over
             </div>
             <div className={'blob'} style={{position: 'absolute', width: '150vw', height: '60vh', marginTop: '15rem'}}></div>
         <List id="prizes">
-            <li style={{fontSize: '6rem', color:'#e25625', fontWeight: 800, fontFamily: 'Futura'}}>Prémios:</li>
+            <li style={{fontSize: '6rem', color:'#e25625', fontWeight: 800, fontFamily: 'Futura', marginLeft: '0px'}}>Prémios:</li>
             {arr.map((item, index) => (
-                <li key={index} className={item ? 'prize' : ''} style={{marginLeft: '-5vw'}}>
+                <li key={index} className={item ? 'prize' : ''} style={{textAlign: 'left'}}>
                     {index + 1}. {item ? item : 'Sem prémio'}
                 </li>
             ))}
@@ -25,6 +25,7 @@ const List = styled.ul`
     line-height: 14rem;
 
     li {
+    
         color: #fff;
     }
 `;
